d4d4b34faaed129c2146a15475b679a5e4ee8e0d
rilieo <rilesdou@gmail.com>
2024-05-27 00:42:59 -0500

    configure table views for DetailViewController

--- yourway/yourway/DetailViewController.swift
+++ yourway/yourway/DetailViewController.swift
@@ -7,19 +7,50 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
-    
+struct TrainInfo {
+    let time : String
+    let line : String
+}
 
-    @IBOutlet weak var trainLineN: UITextView!
-    @IBOutlet weak var trainTimeN: UITextView!
-    @IBOutlet weak var trainLineS: UITextView!
-    @IBOutlet weak var trainTimeS: UITextView!
+class DetailViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (tableView == uptownTrainsTableView) {
+            return NStations.count
+        }
+        
+        return SStations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if (tableView == uptownTrainsTableView) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "uptownTableViewCell", for: indexPath) as! UptownTableViewCell
+            
+            cell.trainLine.text = NStations[indexPath.row].line
+            cell.trainTime.text = NStations[indexPath.row].time
+            return cell
+        }
+            
+        let cell = tableView.dequeueReusableCell(withIdentifier: "downtownTableViewCell", for: indexPath) as! DowntownTableViewCell
+        
+        cell.trainLine.text = SStations[indexPath.row].line
+        cell.trainTime.text = SStations[indexPath.row].time
+        
+        return cell
+        
+    }
+    
+    @IBOutlet weak var uptownTrainsTableView: UITableView!
+    @IBOutlet weak var downtownTrainsTableView: UITableView!
     var stop: Station!
+    
+    private var NStations : [TrainInfo] = []
+    private var SStations : [TrainInfo] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         fetchStops()
+        uptownTrainsTableView.dataSource = self
+        downtownTrainsTableView.dataSource = self
     }
     
     private func fetchStops() {
@@ -45,50 +76,55 @@ class DetailViewController: UIViewController {
 
             do {
                 let subwayStops = try JSONDecoder().decode(Stop.self, from: data)
-                
                 let stationData = subwayStops.data
+                var north : [TrainInfo] = []
+                var south : [TrainInfo] = []
                 
-                // Iterate through all 5 stations
+                // Iterate through all stations from stationData and display these stations
                 for station in stationData {
                     if (station.name == self.stop.name) {
+                        // Get next "northbound" train
+                        let nStations = station.N
                         
-                        // Display next train
-                        if let currNStation = station.N.first {
+                        for nStation in nStations {
+                            let timestamp = nStation.time.parseTime()
                             
-                            let timestamp = currNStation.time.parseTime()
-
                             // Parse time
-                            var route = currNStation.route
-                            let charset = CharacterSet(charactersIn: "1234567")
-                            if route.rangeOfCharacter(from: charset) != nil {
-                                route = currNStation.route.replacingOccurrences(of: #"[^\d\s]"#, with: "", options:     .regularExpression)
-                                }
-                            
-                            DispatchQueue.main.async { [weak self] in
-                                self?.trainTimeN.text = timestamp
-                                self?.trainLineN.text = route
+                            var route = nStation.route
+                            if route.rangeOfCharacter(from: CharacterSet(charactersIn: "1234567")) != nil {
+                                route = nStation.route.replacingOccurrences(of: #"[^\d\s]"#, with: "", options:     .regularExpression)
                             }
+                            
+                            north.append(TrainInfo(time: timestamp, line: route))
                         }
                         
-                        if let currSStation = station.S.first {
-                            let timestamp = currSStation.time.parseTime()
+                        // Get next "southbound" train
+                        let sStations = station.S
+                        
+                        for sStation in sStations {
+                            let timestamp = sStation.time.parseTime()
                             
-                            var route = currSStation.route
-                            let charset = CharacterSet(charactersIn: "1234567")
-                            if route.rangeOfCharacter(from: charset) != nil {
-                                route = currSStation.route.replacingOccurrences(of: #"[^\d\s]"#, with: "", options:     .regularExpression)
+                            // Parse time
+                            var route = sStation.route
+                            if route.rangeOfCharacter(from: CharacterSet(charactersIn: "1234567")) != nil {
+                                route = sStation.route.replacingOccurrences(of: #"[^\d\s]"#, with: "", options:     .regularExpression)
                             }
                             
-                            DispatchQueue.main.async { [weak self] in
-                                self?.trainTimeS.text = timestamp
-                                self?.trainLineS.text = route
-                            }
+                            south.append(TrainInfo(time: timestamp, line: route))
                         }
+                        
+                        DispatchQueue.main.async { [weak self] in
+                            self?.NStations = north
+                            self?.SStations = south
+                            self?.uptownTrainsTableView.reloadData()
+                            self?.downtownTrainsTableView.reloadData()
+                        }
+                        
                     }
                 }
-
             } catch {
-                print("❌ Error decoding JSON: \(error)")
+                print("Error decoding JSON: \(error)")
+                return
             }
         }
         session.resume()
@@ -109,7 +145,6 @@ extension String {
                 let range = Range(match.range, in: self)!
                 let time = String(self[range])
                 return time
-                
             }
         } catch {
             print("Could not parse")
