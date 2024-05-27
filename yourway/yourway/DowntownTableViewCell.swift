b73787103487359fde4ffa1593cda2e8f13a813d
rilieo <rilesdou@gmail.com>
2024-05-27 00:34:45 -0500

    add tableViewCells for downtown and uptown trains

---
+++ yourway/yourway/DowntownTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DowntownTableViewCell.swift
+//  yourway
+//
+//  Created by Riley Dou on 5/24/24.
+//
+
+import UIKit
+
+class DowntownTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ yourway/yourway/UptownTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UptownTableViewCell.swift
+//  yourway
+//
+//  Created by Riley Dou on 5/24/24.
+//
+
+import UIKit
+
+class UptownTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
