03a5476a9cff980e7de23f94bde4a36887584a4b
rilieo <rilesdou@gmail.com>
2024-05-29 01:30:43 -0500

    add search functionality

--- yourway/yourway/ViewControllers/SearchViewController.swift
+++ yourway/yourway/ViewControllers/SearchViewController.swift
@@ -8,6 +8,8 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var stopSearchBar: UISearchBar!
     
     var allStations: [Station] = []
+    var searchStations: [Station] = []
+    var searching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,16 +19,30 @@ class SearchViewController: UIViewController {
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (searching) {
+            return searchStations.count
+        }
+        
         return allStations.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "allStationsTableViewCell", for: indexPath) as! AllStationsTableViewCell
         
-        cell.allStationsLabel.text = allStations[indexPath.row].name
+        if (searching) {
+            cell.allStationsLabel.text = searchStations[indexPath.row].name
+        } else {
+            cell.allStationsLabel.text = allStations[indexPath.row].name
+        }
         
         return cell
     }
-    
-    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchStations = allStations.filter({$0.name.prefix(searchText.count) == searchText})
+        searching = true
+        stationsTableView.reloadData()
+    }
 }
